04f20529ec439e3fc8a61449b1b3d10767953293
Santiago Nieva Glembocki <146479304+SantiNG23@users.noreply.github.com>
2025-08-13 17:31:31 -0300

    Errores del Linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout.jsx';
 import Home from './pages/Home.jsx';
 import Explore from './pages/Explore.jsx';
@@ -10,8 +10,6 @@ import ImageOptimizer from './pages/ImageOptimizer.jsx';
 
 export default function App() {
   return (
-    
-    
     <Routes>
       <Route element={<MainLayout />}>
         <Route index element={<Home />} />
